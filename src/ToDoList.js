7854405ac2e3daca50662c9658b9af41b40c8b55
Boian Etropolski <boetropolski@gmail.com>
2018-04-12 12:13:50 -0500

    implement add items feature

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,19 +1,52 @@
 import React, { Component } from "react";
- 
-class ToDoList extends Component {
+import TodoItems from "./TodoItems"
+
+class TodoList extends Component {
+
+   constructor(props) {
+       super(props);
+       this.addItem = this.addItem.bind(this);
+       this.state = {
+           items: []
+       };
+   }
+
+   addItem(e) {
+       if (this._inputElement.value !== "") {
+         var newItem = {
+           text: this._inputElement.value,
+           key: Date.now()
+         };
+     
+         this.setState((prevState) => {
+           return {
+             items: prevState.items.concat(newItem)
+           };
+         });
+       
+         this._inputElement.value = "";
+       }
+       
+       console.log(this.state.items);
+         
+       e.preventDefault();
+     }
+
  render() {
    return (
      <div className="todoListMain">
        <div className="header">
-         <form>
-           <input placeholder="enter task">
+         <form onSubmit={this.addItem}>
+           <input ref={(a) => this._inputElement = a}
+             placeholder="enter task">
            </input>
-           <button type="submit">add</button>
+           <button type="submit">add5</button>
          </form>
+            <TodoItems entries={this.state.items}/>
        </div>
      </div>
    );
- } 
+ }
 }
 
-export default ToDoList;
+export default TodoList;
